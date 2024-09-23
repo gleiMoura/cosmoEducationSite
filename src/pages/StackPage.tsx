72a7eeca10621c98b141809f4e66a53dea7f66b7
gleiMoura <gle.bar.mou@gmail.com>
2024-09-23 15:33:15 -0300

    🚧 creating a new card

--- src/pages/StackPage.tsx
+++ src/pages/StackPage.tsx
@@ -1,7 +1,6 @@
 import { FC, useContext } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
-import Icon from "../components/Icon";
 import Title from "../components/Title";
 import { ModeContext } from "../contexts/ModeContext";
 
@@ -12,28 +11,20 @@ export const Stack: FC = () => {
 
     return (
         <Main mode={mode} id="tecnologies">
-            <Title title="Tecnologias" />
+            <Title title="O que falam das minhas aulas?" />
             <Cards>
-                <Card title="Front End" color={mode ? "white" : "#F2C744"} component={
-                    <>
-                        <Icon name="ReactJS" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-                        <Icon name="HTML" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <Icon name="CSS" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-                    </>
-                } />
-                <Card title="Back End" color={mode ? "white" : "#F2C744"} component={
-                    <>
-                        <Icon name="MongoDB" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
-                        <Icon name="PostgreSQL" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
-                        <Icon name="Express" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-                    </>
-                } />
-                <Card title="Complementares" color={mode ? "white" : "#F2C744"} component={
-                    <>
-                        <Icon name="Git" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
-                        <Icon name="Jest" size={70} link="https://cdn.iconscout.com/icon/free/png-512/free-jest-3521517-2945020.png?f=avif&w=256" />
-                        <Icon name="Docker" size={70} link="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
-                    </>
+                <Card color={mode ? "white" : "gray"} component={
+                    <Content>
+                        <h1 className="name">
+                            Juliana
+                        </h1>
+                        <p className="text">
+                            Eu adoro o Gleison
+                        </p>
+                        <div className="start">
+                            Eu sou uma estrelinha
+                        </div>
+                    </Content>
                 } />
             </Cards>
         </Main>
@@ -49,7 +40,6 @@ const Main = styled.main<{ mode: boolean }>`
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    background-image: url(${(props) => props.mode ? '/images/fundo.png' : '/images/fundo_claro.png'});
     background-size: 'cover';
     background-position: 'center';
     background-color: ${(props) => props.mode ? "none" : "white"};
@@ -57,8 +47,33 @@ const Main = styled.main<{ mode: boolean }>`
 
 const Cards = styled.div`
     width: 100%;
+    padding: 20px;
+    box-sizing: border-box;
     flex-wrap: wrap;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
+`;
+
+const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    position: relative;
+
+    h1{
+        font-size: 1.2rem;
+        font-weight: 500;
+    }
+
+    p{
+        font-size: 1rem;
+    }
+
+    .stars{
+        
+    }
 `
