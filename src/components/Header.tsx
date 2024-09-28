2aa62b2dbec51bc8838c2cc95d0671bb039a7ad6
gleiMoura <gle.bar.mou@gmail.com>
2024-09-28 18:34:41 -0300

    ✨ feat: create a theme to use in project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { IoSunnyOutline } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
 import { FC } from "react";
+import { theme } from "../theme";
 
 interface HeaderProps {
     changeMode: () => void,
@@ -29,7 +30,6 @@ const Header: FC<HeaderProps> = ({ changeMode, mode }) => {
                 changeMode()
             }}>
                 {mode ? <IoMoon size={30} /> : <IoSunnyOutline size={30} />}
-
             </Mode>
         </HeaderMain>
     )
@@ -38,7 +38,7 @@ const Header: FC<HeaderProps> = ({ changeMode, mode }) => {
 const HeaderMain = styled.header`
     width: 100%;
     height: 80px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.173);
     position: fixed;
     top: 0;
     left: 0;
@@ -47,7 +47,7 @@ const HeaderMain = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #F2C744;
+    background-color: ${theme.colors.black};
 
     .links {
         width: 100%;
@@ -56,9 +56,9 @@ const HeaderMain = styled.header`
     }
 
     a{
-        color: black;
+        color: white;
         font-weight: 500;
-        font-size: 1.5rem;
+        font-size: 1rem;
         font-family: 'Roboto';
         cursor: pointer;
     }
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        yellow: "#FED500",
+        green: "#00E88F",
+        background:"#FFFFFF",
+        black: "#333333",
+        purple: "#B466FA"
+    }
+}
