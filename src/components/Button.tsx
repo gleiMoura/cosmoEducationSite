cd0c0bfda03aed737445475febf178286adf7957
gleiMoura <gle.bar.mou@gmail.com>
2024-09-29 14:08:06 -0300

    ✨ feat: create a new component Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react"
+import styled from "styled-components";
+import { theme } from "../theme";
+
+interface propsButton {
+    text: string,
+    link: string
+}
+
+export const Button: FC<propsButton> = ({ text, link }) => {
+    return (
+        <Main target="_blank" href={link}>
+            {text}
+        </Main>
+    )
+};
+
+const Main = styled.a`
+        width: 200px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 20px 0;
+        border-radius: 10px;
+        border: none;
+        background-color: ${theme.colors.yellow};
+        color: black;
+        font-family: 'Roboto';
+        font-size: 1.2rem;
+        font-weight: 600;
+        text-decoration: none;
+        cursor: pointer;
+
+        &:hover{
+        background-color: ${theme.colors.purple};
+        color: black;
+   }
+`
