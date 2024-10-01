16d564b20c2ef023ef4ce7185b71084c5c11e674
gleiMoura <gle.bar.mou@gmail.com>
2024-10-01 17:25:08 -0300

    🔥 remove unused code

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useContext } from "react"
+import { FC, useContext } from "react"
 import { FaArrowCircleRight } from "react-icons/fa";
 import { FaPerson } from "react-icons/fa6";
 import { MdLiveTv } from "react-icons/md";
