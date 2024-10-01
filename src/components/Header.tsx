41c874dcaa3c6b260d60c5734b61e9269226732c
gleiMoura <gle.bar.mou@gmail.com>
2024-10-01 17:21:04 -0300

    ✨ fear: create a toggle bar to mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { IoSunnyOutline } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { theme } from "../theme";
+import { FaBars } from "react-icons/fa";
+
 
 interface HeaderProps {
     changeMode: () => void,
@@ -10,9 +12,18 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({ changeMode, mode }) => {
+    const [toggleBar, setToggleBar] = useState(false);
+
+    const ChangeToggle = () => {
+        setToggleBar(!toggleBar)
+    };
+
     return (
-        <HeaderMain>
-            <div className="links">
+        <HeaderMain mode={mode}>
+            <div className="mode invisible" onClick={ChangeToggle}>
+                <FaBars />
+            </div>
+            <div className={toggleBar ? "mobile_links" : "links"}>
                 <a onClick={() => {
                     document.getElementById("presentation")
                         ?.scrollIntoView({ behavior: "smooth" })
@@ -30,16 +41,16 @@ const Header: FC<HeaderProps> = ({ changeMode, mode }) => {
                         ?.scrollIntoView({ behavior: "smooth" })
                 }}>informações úteis</a>
             </div>
-            <Mode mode={mode} onClick={() => {
+            <div className="mode" onClick={() => {
                 changeMode()
             }}>
                 {mode ? <IoMoon size={30} /> : <IoSunnyOutline size={30} />}
-            </Mode>
+            </div>
         </HeaderMain>
     )
 };
 
-const HeaderMain = styled.header`
+const HeaderMain = styled.header<{ mode: boolean }>`
     width: 100%;
     height: 80px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.173);
@@ -60,6 +71,22 @@ const HeaderMain = styled.header`
         justify-content: space-evenly;
     }
 
+    .mobile_links{
+        width: 200px;
+        height: 200px;
+        padding: 30px;
+        box-sizing: border-box;
+        background-color: black;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 10px;
+        color: white;
+        position: absolute;
+        top: 80px;
+    }
+
     a{
         color: white;
         font-weight: 500;
@@ -72,24 +99,34 @@ const HeaderMain = styled.header`
         color: #929191
     }
 
-    @media(max-width: 950px) {
+    .mode{
+        width: 80px;
+        height: 50px;
+        background-color: ${(props) => props.mode ? "#21272F" : "white"};
+        color: ${(props) => props.mode ? "white" : "#21272F"};
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    }
+
+    .invisible{
+        display: none;
+    }
+
+    @media(max-width: 600px) {
         a{
             font-size: 1rem;
         }
-    }
-`;
+        .links{
+            display: none;
+        }
 
-const Mode = styled.div<{ mode: boolean }>`
-    width: 80px;
-    height: 50px;
-    background-color: ${(props) => props.mode ? "#21272F" : "white"};
-    color: ${(props) => props.mode ? "white" : "#21272F"};
-    border-radius: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    
+        .invisible{
+            display: flex;
+        }
+    }
 `;
 
 export default Header;
